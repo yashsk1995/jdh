21b1083419dbeaba5a092a68e7ab97f98d57cded
yashK <yashkinariwalask95@gmail.com>
2020-01-15 18:32:20 -0500

    boiler-done

---
+++ assets/PHP_Templates/nav.php
@@ -0,0 +1,38 @@
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="./index.php">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="./index.php">Home</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Portfolio
+          <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="./currentonsale.php">Currently on Sale</a></li>
+            <li><a href="./construction.php">Construction-renovation</a></li>
+            <li><a href="./pastfolio.php">Pastfolio</a></li> 
+          </ul>
+        </li>
+        <li><a href="./aboutus.php">Our Story</a></li>
+        <li><a href="./contact.php">Contact</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Login
+          <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Invester</a></li>
+            <li><a href="#">Manager</a></li>
+            <li><a href="#">JDH</a></li> 
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ construction.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <title>Bootstrap Theme The Band</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="./assets/CSS/Theme.css" />
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+
+     <?php include_once ('./assets/PHP_Templates/nav.php'); ?> 
+     <h1>Code Start here for contsruction </h1>
+     <?php include_once ('./assets/PHP_Templates/footer.php'); ?>
+</body>
+</html>
